1574e8e8f4423deb6f5589078584b6756530ff67
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2020-05-09 15:17:43 +0300

    Initial Commit

---
+++ MultoDimensionalArrayDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MultoDimensionalArrayDemo
+//
+//  Created by Водолазкий В.В. on 09.05.2020.
+//  Copyright © 2020 Водолазкий В.В. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
